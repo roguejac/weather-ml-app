360fec49904b36231b320a37f00d430eab8c9ef1
roguejac <46568234+roguejac@users.noreply.github.com>
2025-06-06 09:25:35 +0200

    Update api_fetcher.py

--- api_fetcher.py
+++ api_fetcher.py
@@ -1,6 +1,6 @@
 import requests
-
-API_KEY = "c1e3b246b33fdfa45ed8ad78e671d520"  # Replace with your OpenWeatherMap API key
+import os
+API_KEY = os.environ.get("API_KEY")  # Replace with your OpenWeatherMap API key
 
 def get_weather(city="Johannesburg"):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
